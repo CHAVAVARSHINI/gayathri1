f3e96edc49aa50cedf1cce14f69ccf5e68e776c6
CHAVAVARSHINI <chavavarshini9@gmail.com>
2025-03-06 11:33:23 +0530

    Update Box1.java
    bye

--- Box1.java
+++ Box1.java
@@ -1,14 +1,4 @@
-class Box { 
-	double width;
-	 double height; 
-	double depth;      // compute and return volume 
-	double volume() { 
-		return width * height * depth;
-	 } 
-// sets dimensions of box 
-	void setDim(double w, double h, double d) {
-		 width = w;
-		height = h; 
+hi hiiiiiiiiiiiiiiiiii
 		depth = d;
  	}
  }
